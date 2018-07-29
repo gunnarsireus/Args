9dae30b2cb52b955170036f8350a7741c48e0fe7
Gunnar Siréus <gunnar.sireus@gmail.com>
2018-07-29 21:28:43 +0200

    Finetuning

--- Args/Program.cs
+++ Args/Program.cs
@@ -1,5 +1,6 @@
 ﻿using com.cleancoder.args;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 
 namespace com.cleancoder.args
@@ -10,22 +11,25 @@ namespace com.cleancoder.args
         {
             try
             {
-                Args arg = new Args("l,p#,d*", new string[3] { "-l", "-37", "-hejsan" });
+                var arg = new Args("l,p#,d*", args);
                 bool logging = arg.getBoolean('l');
                 int port = arg.getInt('p');
                 string directory = arg.getString('d');
-                executeApplication(logging, port, directory);
+                ExecuteApplication(logging, port, directory);
             }
             catch (ArgsException e)
             {
                 Console.Write("Argument error: {0}\n", e.errorMessage());
+                Console.ReadKey();
             }
         }
-        private static void executeApplication(bool logging, int port, string directory)
+        private static void ExecuteApplication(bool logging, int port, string directory)
         {
             Console.Write("logging is {0}, port:{1:D}, directory:{2}\n", logging, port, directory);
+            var args = new Args("f&", new string[] { "-f", "key1:val1" });
+            IDictionary<string, string> map = args.getMap('f');
+            Console.Write("map[key1]: " + map["key1"]);
             Console.ReadKey();
-            Main(new string[13] { "-f", "-s", "Bob", "-n", "1", "-a", "3.2", "-p", "e1", "-p", "e2", "-p", "e3" });
         }
     }
 }
